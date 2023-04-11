f00329da7f3747e1e59558b5dabc1349baf01c46
Ankit-Dhattarwal <ankitdhatterwal09@gmail.com>
2023-04-11 11:04:25 +0530

    homzy first

---
+++ lib/small_serice/booked_service.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class bookSubPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text(
+          'No Service!',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 24,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+
+      ),
+    );
+  }
+}
